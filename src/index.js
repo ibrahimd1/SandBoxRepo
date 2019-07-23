038ae39676119f436dd304b507f5c99259574bfc
ibrahimd1 <37157114+ibrahimd1@users.noreply.github.com>
2019-07-23 18:10:17 +0300

    todos
    todos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,92 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
+import "./base.css";
+import "./index.css";
+
+function Footer() {
+  return (
+    <footer id="footer" style={{ display: "block" }}>
+      <span id="todo-count">
+        <strong>5</strong> items left
+      </span>
+      <ul id="filters">
+        <li>
+          <a className="selected" href="#/">
+            All
+          </a>
+        </li>
+        <li>
+          <a href="#/active">Active</a>
+        </li>
+        <li>
+          <a href="#/completed">Completed</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+function ToDoListItem(props) {
+  return (
+    <li>
+      <div className="view">
+        <input
+          //className="toggle"
+          type="checkbox"
+          checked={props.items.completed}
+        />
+        <label>{props.items.description}</label>
+        <button className="destroy" />
+      </div>
+      <input className="edit" defaultValue={props.items.description} />
+    </li>
+  );
+}
+
+function TodoList(props) {
+  const todosView = [];
+  for (let i = 0; i < props.items.length; i++) {
+    todosView.push(<ToDoListItem items={props.items[i]} />);
+  }
+
+  return <ul id="todo-list">{todosView}</ul>;
+}
+
+function Header() {
+  return (
+    <header id="header">
+      <h1>todos</h1>
+      <input id="new-todo" placeholder="What needs to be done?" autofocus />
+    </header>
+  );
+}
+
+class ToDoApp extends React.Component {
+  state = {
+    todos: [
+      { id: 1, description: "Buy tomatos", completed: false },
+      { id: 2, description: "Buy potatos", completed: false },
+      { id: 3, description: "Buy deneme", completed: true }
+    ],
+    currentFilter: "ALL",
+    tempToDo: ""
+  };
+
+  render() {
+    return (
+      <section id="todoapp">
+        <Header />
+        <section id="main" style={{ display: "block" }}>
+          <input id="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <TodoList items={this.state.todos} />
+        </section>
+        <Footer />
+      </section>
+    );
+  }
+}
 
 class Sayac extends React.Component {
   state = { sayac: 0 };
@@ -35,8 +120,9 @@ function App() {
   return (
     <div className="App">
       {/*<h1>Hello merhaba nasılsın CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>*/}
-      <Sayac />
+      <h2>Start editing to see some magic happen!</h2> <Sayac />
+      */}
+      <ToDoApp />
     </div>
   );
 }
